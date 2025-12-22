5eeae9561237a062f91383b15e670a4b36d9aaf6
Henry <hello264987@gmail.com>
2025-12-22 16:42:41 +0800

    學習上傳

---
+++ 003.變量/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //創建變量
+            //創建一個數據的容器，容器的名字叫做age，容器的類型是int
+            //聲明一個變量，變量的名字是age
+            int age; //int變量類型 age變量名
+            //往容器裡面放東西  賦值
+            age = 10;
+
+            int a = 3, b = 8;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + "+" + b);
+            Console.WriteLine(+a);
+
+
+        }
+    }
+}
--- 004-字符類型/obj/Debug/net10.0/004-字符類型.AssemblyInfo.cs
+++ 004-字符類型/obj/Debug/net10.0/004-字符類型.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("001-開始")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("004-字符類型")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f110cde85572b38318575175df1903ed78483fa7")]
-[assembly: System.Reflection.AssemblyProductAttribute("001-開始")]
-[assembly: System.Reflection.AssemblyTitleAttribute("001-開始")]
+[assembly: System.Reflection.AssemblyProductAttribute("004-字符類型")]
+[assembly: System.Reflection.AssemblyTitleAttribute("004-字符類型")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 類別產生。

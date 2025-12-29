b1dd65b53b115ad8a23e7ee2e063a6f28c594286
Henry <hello264987@gmail.com>
2025-12-29 17:32:53 +0800

    到23，學習輸入和輸出

--- 004-字符類型/Program.cs
+++ 004-字符類型/Program.cs
@@ -6,6 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            /*
             //設定路徑
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);  //設定成桌面
             string filePath = Path.Combine(desktopPath, "LogData.csv"); //把多段路徑組合成正確、可跨平台的完整路徑
@@ -19,6 +20,20 @@ namespace MyApp
                 sw.WriteLine($"第二排,第二格");
                 sw.WriteLine($"第三排,第二格");
             }
+            */
+
+            //怎麼讀取數據 輸入數據
+            //string str = Console.ReadLine();   //"12" 12  這兩個其實是不一樣的，一個是字符的 一個是數字的
+            //Console.WriteLine(str + "-");
+            //1.類型一致 2.右邊的值所需要的容器大小 小於等於左邊的容器
+            //int a = Console.ReadLine();
+
+            string str = Console.ReadLine();
+            int strInt = Convert.ToInt32(str);
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(strInt + "-" + a);
         }
 
         private static void InitializeCsv(string path)
